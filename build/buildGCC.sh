ce8f1b6e012a0cca534f9ffb4e50c4a20bc6c987
Samuel Jenness <smjenness@gmail.com>
2019-03-21 20:05:03 -0400

    Update build scripts

--- build/buildGCC.sh
+++ build/buildGCC.sh
@@ -2,5 +2,4 @@
 
 . /gscratch/csde/sjenness/spack/share/spack/setup-env.sh
 spack load gcc@8.2.0
-# spack install -j1 r ^jdk@10.0.2_13 %gcc@8.2.0
-spack install -j1 r %gcc@8.2.0
+spack install -j1 gcc %gcc@8.2.0
---
+++ build/scp.sh
@@ -0,0 +1,8 @@
+
+# Build R files on Ikt
+scp build/buildR.master.sh hyak:~/
+scp build/buildR.sh hyak:~/
+
+# Build R files on Mox
+scp build/buildR.master.sh mox:~/
+scp build/buildR.sh mox:~/
