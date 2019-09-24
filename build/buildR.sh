decbbb8eaa130a5425b1cfda8eac57f919b2d11f
Samuel Jenness <smjenness@gmail.com>
2019-09-24 12:42:03 -0400

    Update buildR.sh

--- build/buildR.sh
+++ build/buildR.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. /gscratch/csde/sjenness/spack/share/spack/setup-env.sh
+. /gscratch/csde/spack/spack/share/spack/setup-env.sh
 spack load gcc@8.2.0
 # spack install -j1 r ^jdk@10.0.2_13 %gcc@8.2.0
 spack install -j1 r %gcc@8.2.0
