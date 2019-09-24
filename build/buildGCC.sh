21eb0a067818e7c4d7fd032eab138526bdd95a58
Samuel Jenness <smjenness@gmail.com>
2019-09-24 12:42:30 -0400

    Update buildGCC.sh

--- build/buildGCC.sh
+++ build/buildGCC.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-. /gscratch/csde/sjenness/spack/share/spack/setup-env.sh
+. /gscratch/csde/spack/spack/share/spack/setup-env.sh
 spack load gcc@8.2.0
 spack install -j1 gcc %gcc@8.2.0
